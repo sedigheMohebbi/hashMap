85617ffbfac909f63d9aa92c9fb9cbdc09027a14
sedighe <bit.mohebbi@gmail.com>
2015-08-13 12:56:52 +0430

    commit 2

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,5 @@
 
-public class Student implements Comparable<Student > {
+public class Student implements Comparable<Student> {
     private String name;
     private Integer Average;
 
@@ -26,10 +26,11 @@ public class Student implements Comparable<Student > {
 
     @Override
     public int compareTo(Student student) {
-        return  this.getAverage().compareTo(student.getAverage());
+
+        return this.getAverage().compareTo(student.getAverage());
     }
-    public String toString(){
-        return getName()+" "+getAverage();
+    public String toString() {
+        return getName() + " " + getAverage();
     }
 
 }
