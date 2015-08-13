aff0a5f6fbdac8f8ab07ea8d5218d8fdf37d247a
sedighe <bit.mohebbi@gmail.com>
2015-08-13 09:24:02 +0430

    commit 2

--- src/MainStudentAverageSorter.java
+++ src/MainStudentAverageSorter.java
@@ -3,20 +3,19 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
-// project hashmap test
 public class MainStudentAverageSorter {
     public static void main(String[] args) {
         HashMap<String, Integer> stringIntegerHashMap = new HashMap<String, Integer>();
-        stringIntegerHashMap.put("student1",12);
-        stringIntegerHashMap.put("student2",14);
-        stringIntegerHashMap.put("student3",15);
-        stringIntegerHashMap.put("student4",18);
+        stringIntegerHashMap.put("student1", 12);
+        stringIntegerHashMap.put("student2", 14);
+        stringIntegerHashMap.put("student3", 15);
+        stringIntegerHashMap.put("student4", 18);
 
         StudentAverageSorter studentAverageSorter = new StudentAverageSorter();
-        List studentList= studentAverageSorter.sorByAverage(stringIntegerHashMap);
-        Iterator iterator=studentList.iterator();
+        List studentList = studentAverageSorter.sorByAverage(stringIntegerHashMap);
+        Iterator iterator = studentList.iterator();
         while (iterator.hasNext()) {
-            System.out.println("list of average :\n " +iterator.next());
+            System.out.println("list of average :\n " + iterator.next());
         }
 
     }
